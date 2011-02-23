74248f032bf9cb577ad157023afd018d2119ac43
Thomas Baldwin <tbaldwin@uoregon.edu>
2011-02-23 14:23:49 -0800

    Added check for presence of PyVISA

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -5,6 +5,16 @@
 # tkb
 
 from spex750m import spex750m
-from triax import triax
-from egg5110 import egg5110
-from agilent_signal_generators import ag8648
+
+# Some instruments depend on PyVISA, which in turn
+# relies on the NI VISA stack. This is unavailable
+# on some systems, so only make these instruments
+# available on those platforms.
+try:
+    import visa
+except ImportError:
+    print "No VISA bindings found. GPIB unavailable."
+else:
+    from triax import triax
+    from egg5110 import egg5110
+    from agilent_signal_generators import ag8648
