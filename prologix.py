cc463897ba5dbdf3bbf3db996312b0db29a4400c
Thomas Baldwin <tbaldwin@uoregon.edu>
2011-07-28 16:50:00 -0700

    Added support for ++savecfg, prologix logging
    To avoid wearing out the EEPROM, the prologix code now turns off configuration saving. Otherwise there is an EEPROM write every time the address is switched.
    
    The prologix code also logs everything written and read, for debugging. It seems to be working fine, however. This functionality should be turn-offable and probably incorporated into my Serial subclass instead.

--- prologix.py
+++ prologix.py
@@ -1,5 +1,5 @@
 from wanglib.util import Serial
-from time import sleep
+from time import sleep, time
 
 class prologix_USB(object):
     """
@@ -7,25 +7,42 @@ class prologix_USB(object):
 
     """
 
-    def __init__(self, port='/dev/ttyUSB4'):
+    def __init__(self, port='/dev/ttyUSBgpib'):
+        # open log file
+        self.lf = open('prologix.log', 'a')
+        self.start = time()
+        self.lf.write('\nplx initialized at %.2f\n\n' % self.start)
+
         # create a serial port object
-        self.bus = Serial(port)
+        self.bus = Serial(port, baudrate=115200, rtscts=1)
+        # if this doesn't work, try settin rtscts=0
+
+        # flush whatever is hanging out in the buffer
         self.bus.readall()
+        # don't save settings (to avoid wearing out EEPROM)
+        self.savecfg = False
 
         # keep a local copy of the current address
         # so we're not always asking for it
         self._addr = self.addr
 
-    def write(self, command):
+    def clock(self):
+        return time() - self.start
+
+    def write(self, command, lag=0.5):
         self.bus.write("%s\r" % command)
+        self.lf.write('%.2f write: %s\n' % (self.clock(), command))
+        sleep(lag)
 
     def readall(self):
-        return self.bus.readall().rstrip()
+        resp = self.bus.readall()
+        self.lf.write('%.2f read: ' % self.clock())
+        self.lf.write(resp)
+        return resp.rstrip()
 
-    def ask(self, query, lag=0.1):
+    def ask(self, query, *args, **kwargs):
         """ Write to the bus, then read response. """
-        self.write(query)
-        sleep(lag)
+        self.write(query, *args, **kwargs)
         return self.readall()
 
     # use addr to select an instrument by its GPIB address
@@ -49,6 +66,18 @@ class prologix_USB(object):
     def version(self):
         """ Query the Prologix firmware version """
         return self.ask("++ver")
+
+    @property
+    def savecfg(self):
+        resp = self.ask("++savecfg")
+        return bool(int(resp))
+    @savecfg.setter
+    def savecfg(self, val):
+        d = bool(val)
+        self.write("++savecfg %d" % d)
+
+
+
 
 
 class instrument(object):
@@ -92,3 +121,4 @@ class instrument(object):
     def write(self, command):
         self.get_priority()
         self.controller.write(command)
+
