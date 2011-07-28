37d5a92c37b26f897b65f24b9c25e66b062b82e7
Thomas Baldwin <tbaldwin@uoregon.edu>
2011-07-28 16:53:37 -0700

    Removed old triax code
    This hasn't been used in a while. The triax320 class, which implements rs232 communication (instead of gpib) is much more stable.

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -9,7 +9,3 @@ from lockins import egg5110, srs830
 from stages import ESP300_stage
 from signal_generators import ag8648
 
-# this is the old triax code, designed for GPIB
-# communication. don't use it, it sucks.
-# from triax import triax
-
