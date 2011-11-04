6ab6ed317bcaaf2338e0c5e6ebb9368397220d60
Thomas Baldwin <tbaldwin@uoregon.edu>
2011-11-03 17:15:46 -0700

    Added support for prologix ethernet-GPIB bridge

--- prologix.py
+++ prologix.py
@@ -1,41 +1,24 @@
 from wanglib.util import Serial
+from socket import socket, AF_INET, SOCK_STREAM, IPPROTO_TCP
 from time import sleep
 
-class prologix_USB(object):
+class prologix_base(object):
     """
-    Interface to a prologix usb-gpib controller.
+    Base class for prologix controllers (ethernet/usb)
 
     """
 
-    def __init__(self, port='/dev/ttyUSBgpib', log=False):
-        # create a serial port object
-        self.bus = Serial(port, baudrate=115200, rtscts=1, log=log)
-        # if this doesn't work, try settin rtscts=0
+    def __init__(self):
+        """
+        initialization routines common to USB and ethernet
 
-        # flush whatever is hanging out in the buffer
-        self.bus.readall()
-        # don't save settings (to avoid wearing out EEPROM)
-        self.savecfg = False
-
+        """
         # keep a local copy of the current address
         # and read-after write setting
         # so we're not always asking for it
         self._addr = self.addr
         self._auto = self.auto
 
-    def write(self, command, lag=0.1):
-        self.bus.write("%s\r" % command)
-        sleep(lag)
-
-    def readall(self):
-        resp = self.bus.readall()
-        return resp.rstrip()
-
-    def ask(self, query, *args, **kwargs):
-        """ Write to the bus, then read response. """
-        self.readall()  # clear the buffer
-        self.write(query, *args, **kwargs)
-        return self.readall()
 
     # use addr to select an instrument by its GPIB address
 
@@ -78,6 +61,11 @@ class prologix_USB(object):
     def savecfg(self):
         """ should the controller save settings in EEPROM?  """
         resp = self.ask("++savecfg")
+        if resp == 'Unrecognized command':
+            raise Exception("""
+                Prologix controller does not support ++savecfg
+                update firmware or risk wearing out EEPROM
+                            """)
         return bool(int(resp))
     @savecfg.setter
     def savecfg(self, val):
@@ -93,6 +81,73 @@ class prologix_USB(object):
         """
         return instrument(self, addr, **kwargs)
 
+
+class prologix_ethernet(prologix_base):
+    """
+    Interface to a prologix gpib-ethernet controller.
+
+    """
+
+    def __init__(self, ip='128.223.131.156'):
+        # open a socket to the controller
+        self.bus = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
+        self.bus.settimeout(5)
+        self.bus.connect((ip, 1234))
+
+        # change to controller mode
+        self.bus.send('++mode 1\n')
+
+        # do common startup routines
+        super(prologix_ethernet, self).__init__()
+
+    def write(self, command, lag=0.1):
+        self.bus.send("%s\n" % command)
+        sleep(lag)
+
+    def readall(self):
+        resp = self.bus.recv(100) #100 should be enough, right?
+        return resp.rstrip()
+
+    def ask(self, query, *args, **kwargs):
+        """ Write to the bus, then read response. """
+        # no need to clear buffer
+        self.write(query, *args, **kwargs)
+        return self.readall()
+
+
+class prologix_USB(prologix_base):
+    """
+    Interface to a prologix usb-gpib controller.
+
+    """
+
+    def __init__(self, port='/dev/ttyUSBgpib', log=False):
+        # create a serial port object
+        self.bus = Serial(port, baudrate=115200, rtscts=1, log=log)
+        # if this doesn't work, try settin rtscts=0
+
+        # flush whatever is hanging out in the buffer
+        self.bus.readall()
+
+        # don't save settings (to avoid wearing out EEPROM)
+        self.savecfg = False
+
+        # do common startup routines
+        super(prologix_USB, self).__init__()
+
+    def write(self, command, lag=0.1):
+        self.bus.write("%s\r" % command)
+        sleep(lag)
+
+    def readall(self):
+        resp = self.bus.readall()
+        return resp.rstrip()
+
+    def ask(self, query, *args, **kwargs):
+        """ Write to the bus, then read response. """
+        self.readall()  # clear the buffer
+        self.write(query, *args, **kwargs)
+        return self.readall()
 
 class instrument(object):
     """
@@ -150,10 +205,10 @@ class instrument(object):
         # clear stray bytes from the buffer.
         # hopefully, there will be none.
         # if there are, print a warning
-        clrd = self.controller.bus.inWaiting()
-        if clrd > 0:
-            print clrd, 'bytes cleared'
-        self.read()  # clear the buffer
+#        clrd = self.controller.bus.inWaiting()
+#        if clrd > 0:
+#            print clrd, 'bytes cleared'
+#        self.read()  # clear the buffer
         self.write(command)
         return self.read()
 
@@ -161,6 +216,7 @@ class instrument(object):
         """ read response from instrument.  """
         self.get_priority()
         if not self.auto:
+            pass
             # explicitly tell instrument to talk.
             self.controller.write('++read eoi', lag=self.delay)
         return self.controller.readall()
