13eab748bdb3d9b8d8874423c12c8ab28a675dfb
Thomas Baldwin <tbaldwin@uoregon.edu>
2013-08-06 16:02:26 -0700

    switch to setuptools
    this will make numpy, mpl, and pyserial all auto-install

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+reqs = ['numpy', 'pyserial', 'matplotlib']
+# you may also want (separate pip install):
+#    - PIL (for old SLM stuff)
+#    - scipy (for old fitting helpers, don't use these though)
+#    - gpib (if you use linux, own a NI GPIB card, and feel adventurous)
 
 setup(name='wanglib',
       version='dev',
       description='Instrument control utilities for the Wang lab',
       author='Thomas Baldwin',
       author_email='tbaldwin@uoregon.edu',
+      install_requires=reqs,
       #url='http://no.url.yet',
       packages=['wanglib',
                 'wanglib.instruments',
